9fc492bb861ab140be71c52e0d7e7627bd1ed987
Mantas <latkauskas.mantas@gmail.com>
2019-11-05 15:15:47 +0000

    Initial commit

---
+++ Assets/Scripts/ControllerRotator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(VRTK_ControllerEvents))]
+public class ControllerRotator : MonoBehaviour
+{
+
+    // controller events
+    VRTK_ControllerEvents controlEv;
+    public GameObject playerCamera;
+    public GameObject player;
+    private bool playerIsBig = false;
+
+    void Awake()
+    {
+        // get VRTK controller event component
+        controlEv = GetComponent<VRTK_ControllerEvents>();
+    }
+
+    void OnEnable()
+    {
+        
+        // event listening
+        controlEv.ButtonTwoPressed += RotatePlayer;
+        controlEv.ButtonOnePressed += ScalePlayer;
+    }
+
+    void OnDisable()
+    {
+        // removing event listeners
+       controlEv.ButtonTwoPressed -= RotatePlayer;
+       controlEv.ButtonOnePressed -= ScalePlayer;
+    }
+
+    void RotatePlayer(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("button 2 pressed");
+        playerCamera.transform.Rotate(playerCamera.transform.rotation.x, playerCamera.transform.rotation.y + 180f, playerCamera.transform.rotation.z, Space.Self);
+    }
+
+    void ScalePlayer(object sender, ControllerInteractionEventArgs e)
+    {
+        if (!playerIsBig)
+        {
+            Debug.Log("scalling player up");
+
+            player.transform.localScale += new Vector3(10, 10, 10);
+
+        }
+        else
+        {
+            Debug.Log("scalling player down");
+
+            player.transform.localScale -= new Vector3(10, 10, 10);
+        }
+
+        playerIsBig = !playerIsBig;
+    }
+}
---
+++ Assets/VRTK/Examples/ExampleResources/SceneResources/[005 - Interactions] InteractableObjects/Scripts/TogglePointerInteraction.cs
@@ -0,0 +1,35 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using VRTK.Controllables;
+
+    public class TogglePointerInteraction : MonoBehaviour
+    {
+        public enum OptionType
+        {
+            InteractWithObjects,
+            GrabToPointerTip
+        }
+
+        public OptionType optionType = OptionType.InteractWithObjects;
+        public VRTK_Pointer[] pointers = new VRTK_Pointer[0];
+     
+
+
+        /*protected virtual void OnEnable()
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.clip = audioClip;
+            audio.Play();
+        }*/
+
+        protected virtual void OnDisable()
+        {
+       
+          
+        }
+
+      
+    }
+}
