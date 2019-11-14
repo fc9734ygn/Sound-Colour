aee85713382935d4098c9080b601720d9eedcdea
Mantas <latkauskas.mantas@gmail.com>
2019-11-14 14:40:26 +0000

    added lever prefab

---
+++ Assets/lever.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lever : MonoBehaviour
+{
+
+    public AudioClip audioClip;
+    public GameObject scoreboard;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "leverTrigger")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.clip = audioClip;
+            audio.Play();
+
+            scoreboard.GetComponent<textScript>().IncreaseScore();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "leverTrigger")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.clip = audioClip;
+            audio.Stop();
+        }
+    }
+}
