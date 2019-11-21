2134f4b87ef337cdad43377427dfcc3906d618cc
Bianca9977 <bianca99776@gmail.com>
2019-11-21 15:57:56 +0000

    Jungle Room interactions
    Grabbing the machete and cutting the leaves.

---
+++ Assets/cutLeaf.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutLeaf : MonoBehaviour
+{
+
+    public Transform PrefabLeafCut;
+
+    Transform leafCut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MacheteBlade")
+        {
+           
+            leafCut = Instantiate(PrefabLeafCut, transform.position, Quaternion.identity) as Transform;
+            leafCut.parent = gameObject.transform;
+            leafCut.localRotation = Quaternion.identity;
+            
+            //Destroy(this.gameObject);
+        }
+    }
+}
