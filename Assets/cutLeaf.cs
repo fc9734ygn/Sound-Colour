b662c5e7fc52439e0a047454ba86ec46fbee2715
Bianca9977 <bianca99776@gmail.com>
2019-11-26 12:58:17 +0000

    Leaf cutting and space wall climbing
    All the leaves in the Jungle room have the cutLeaf script and the player in the Space room has the VRTK_Player Climb component.

--- Assets/cutLeaf.cs
+++ Assets/cutLeaf.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class cutLeaf : MonoBehaviour
 {
 
-    public Transform PrefabLeafCut;
+    //public Transform PrefabLeafCut;
 
-    Transform leafCut;
+    //Transform leafCut;
+    //Transform leafWhole;
+    GameObject leafWhole, leafCut;
+    public GameObject PrefabLeafCut;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //leafWhole = this.gameObject.transform;
     }
 
     // Update is called once per frame
@@ -25,12 +28,19 @@ public class cutLeaf : MonoBehaviour
     {
         if (other.gameObject.tag == "MacheteBlade")
         {
-           
-            leafCut = Instantiate(PrefabLeafCut, transform.position, Quaternion.identity) as Transform;
-            leafCut.parent = gameObject.transform;
-            leafCut.localRotation = Quaternion.identity;
-            
-            //Destroy(this.gameObject);
+
+            leafCut = Instantiate(PrefabLeafCut, this.transform.position, this.transform.rotation);
+            leafCut.transform.localScale = this.transform.localScale;
+            //leafCut = Instantiate(PrefabLeafCut, transform.position, Quaternion.identity) as Transform;
+            //leafCut.parent = gameObject.transform;
+            //Debug.Log(" Cut leaf" + leafCut.parent.rotation);
+            //leafCut.localRotation = leafCut.parent.rotation;
+            //leafCut.localScale = new Vector3(1,1,1);
+
+            Debug.Log(" Cut leaf" + gameObject.transform.localScale);
+            //leafCut.localRotation = Quaternion.identity;
+
+            Destroy(this.gameObject);
         }
     }
 }
