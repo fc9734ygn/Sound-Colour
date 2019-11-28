1641f0633ba26bb79e22541beebc1521e6839d44
Alex Newson <a-newson1@hotmail.co.uk>
2019-11-28 15:38:19 +0000

    added interaction in beach room

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadBeach()
+    {
+        SceneManager.LoadScene("CoastalRoom");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CorrectLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectLight : MonoBehaviour
+{
+    public GameObject Light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LightOn()
+    {
+        Light.active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
