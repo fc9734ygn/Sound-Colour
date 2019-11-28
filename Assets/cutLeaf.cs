372a138b5479fe054a66664920ebd06ce2a224e2
Bianca9977 <bianca99776@gmail.com>
2019-11-28 15:59:48 +0000

    Jungle Room interactions
    The machete can be grabbed and used to cut leaves.

--- Assets/cutLeaf.cs
+++ Assets/cutLeaf.cs
@@ -9,7 +9,7 @@ public class cutLeaf : MonoBehaviour
 
     //Transform leafCut;
     //Transform leafWhole;
-    GameObject leafWhole, leafCut;
+    GameObject  leafCut;
     public GameObject PrefabLeafCut;
     // Start is called before the first frame update
     void Start()
@@ -31,13 +31,12 @@ public class cutLeaf : MonoBehaviour
 
             leafCut = Instantiate(PrefabLeafCut, this.transform.position, this.transform.rotation);
             leafCut.transform.localScale = this.transform.localScale;
+           
             //leafCut = Instantiate(PrefabLeafCut, transform.position, Quaternion.identity) as Transform;
             //leafCut.parent = gameObject.transform;
-            //Debug.Log(" Cut leaf" + leafCut.parent.rotation);
             //leafCut.localRotation = leafCut.parent.rotation;
             //leafCut.localScale = new Vector3(1,1,1);
 
-            Debug.Log(" Cut leaf" + gameObject.transform.localScale);
             //leafCut.localRotation = Quaternion.identity;
 
             Destroy(this.gameObject);
