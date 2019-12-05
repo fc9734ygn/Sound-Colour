4f1f3b050918a962533c324609dd947a72443860
Alex Newson <a-newson1@hotmail.co.uk>
2019-12-05 16:00:50 +0000

    Added sounds and central room

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadLevel : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void LoadBeach()
