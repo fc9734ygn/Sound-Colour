b077700cb6bef09360b9fce2b8d3a05ccb92b069
Mantas <latkauskas.mantas@gmail.com>
2019-12-05 15:04:50 +0000

    Arctic room invisible walls, snowball fix,

---
+++ Assets/Scripts/SnowballSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Snowball")
+        {
+            StartCoroutine(SpawnCoroutine());
+        }
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(prefab, transform.position, Quaternion.Euler(0, 0, 0));
+    }
+}
