006e017f942b0126201f9084514b37324b954002
Mantas <latkauskas.mantas@gmail.com>
2019-12-08 23:06:27 +0000

    Added sceneController, added teleportation from main room

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    
+    public void LoadCoastalScene()
+    {
+        SceneManager.LoadScene("CoastalRoom");
+    }
+
+    public void LoadJungleScene()
+    {
+        SceneManager.LoadScene("Jungle");
+    }
+
+    public void LoadArcticScene()
+    {
+        SceneManager.LoadScene("Arctic");
+    }
+
+    public void LoadSpaceScene()
+    {
+        SceneManager.LoadScene("Space");
+    }
+
+    public void LoadMainScene()
+    {
+        var rewardBall = GameObject.Find("RewardBall");
+        DontDestroyOnLoad(rewardBall);
+        SceneManager.LoadScene("CentralRoom");
+    }
+}
