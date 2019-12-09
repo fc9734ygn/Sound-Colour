0f884ab04add546263da62259e73bd95e5303a12
Alex Newson <a-newson1@hotmail.co.uk>
2019-12-09 18:03:01 +0000

    teleport blablabla

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,39 +7,52 @@ public class SceneController : MonoBehaviour
 {
 
     GameObject player;
+
     private void Start()
     {
-      
         player = GameObject.Find("playerFinal");
-        if (player != null)
-        {
-      //      DontDestroyOnLoad(player);
-        }
-
     }
 
     public void LoadCoastalScene()
     {
-        SceneManager.LoadScene("CoastalRoom");
+        player.transform.position = new Vector3(0, 1.5f, 2000);
+        SceneManager.LoadScene("CoastalRoom", LoadSceneMode.Additive);
+        Destroy(GameObject.Find("MainRoom"));
+
     }
 
     public void LoadJungleScene()
     {
-        SceneManager.LoadScene("Jungle");
+        SceneManager.LoadScene("Jungle", LoadSceneMode.Additive);
+        Destroy(GameObject.Find("MainRoom"));
+
     }
 
     public void LoadArcticScene()
     {
-        SceneManager.LoadScene("Arctic");
+        player.transform.position = new Vector3(10000, 1.5f, 0);
+        SceneManager.LoadScene("Arctic", LoadSceneMode.Additive);
+        Destroy(GameObject.Find("MainRoom"));
+
     }
 
     public void LoadSpaceScene()
     {
-        SceneManager.LoadScene("Space");
+        player.transform.position = new Vector3(5000, 1.5f, 5000);
+        SceneManager.LoadScene("Space", LoadSceneMode.Additive);
+        Destroy(GameObject.Find("MainRoom"));
+
     }
 
     public void LoadMainScene()
     {
-        SceneManager.LoadScene("CentralRoom");
+        
+
+        player.transform.position = new Vector3(0, 0, 0);
+        SceneManager.LoadScene("CentralRoom", LoadSceneMode.Additive);
+        Destroy(GameObject.Find("SpaceRoom"));
+        Destroy(GameObject.Find("ArcticRoom"));
+        Destroy(GameObject.Find("JungleRoom"));
+        Destroy(GameObject.Find("CoastalRoom"));
     }
 }
