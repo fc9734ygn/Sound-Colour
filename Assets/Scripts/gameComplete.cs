8510619bbb7c41426353f5df1e6ff0e4af91d66f
Mantas <latkauskas.mantas@gmail.com>
2019-12-10 13:20:02 +0000

    game compl audio

---
+++ Assets/Scripts/gameComplete.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameComplete : MonoBehaviour
+{
+    public GameObject light0;
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+
+    void Update()
+    {
+        if (light0.active && light1.active && light2.active && light3.active)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
