e2d3842b9040fda0a9f3e9acf27908b8675ad667
Alex Newson <a-newson1@hotmail.co.uk>
2019-12-10 15:18:52 +0000

    546

--- Assets/Scripts/gameComplete.cs
+++ Assets/Scripts/gameComplete.cs
@@ -8,11 +8,14 @@ public class gameComplete : MonoBehaviour
     public GameObject light1;
     public GameObject light2;
     public GameObject light3;
+    private bool hasPlayed= false;
 
     void Update()
     {
-        if (light0.active && light1.active && light2.active && light3.active)
+        
+        if (light0.active && light1.active && light2.active && light3.active && !hasPlayed)
         {
+            hasPlayed = true;
             GetComponent<AudioSource>().Play();
         }
     }
